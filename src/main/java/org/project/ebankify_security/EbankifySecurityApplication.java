4c67e8ed95e4b403cea2934bda86798e6392148e
elkhailihamza <elkhailihamza331@gmail.com>
2024-12-02 09:26:30 +0100

    project begin

---
+++ src/main/java/org/project/ebankify_security/EbankifySecurityApplication.java
@@ -0,0 +1,13 @@
+package org.project.ebankify_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankifySecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbankifySecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/ebankify_security/EbankifySecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.ebankify_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankifySecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
